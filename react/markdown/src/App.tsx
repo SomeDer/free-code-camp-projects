adc7d586be3f1075c32c8503fe8bc55df5e80c42
Se De <Sede7473@gmail.com>
2019-08-04 07:53:19 +0100

    Add markdown project

---
+++ react/markdown/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
