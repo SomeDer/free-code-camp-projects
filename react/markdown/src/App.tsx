250a06f1b1810f1d54ccf0e9c4a8138d35e9aeae
Se De <Sede7473@gmail.com>
2019-08-05 07:43:14 +0100

    Finish markdown project

--- react/markdown/src/App.tsx
+++ react/markdown/src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import Container from './Container';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Provider store={store}>
+      <Container />
+    </Provider>
   );
-}
+};
 
 export default App;
